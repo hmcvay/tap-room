44fbca6d6395e0424036709ce037433f9bfd87dd
hmcvay <hayleymcvay8@gmail.com>
2022-04-22 13:44:02 -0700

    1 HOUR LUNCH then added NewTapForm component.

---
+++ src/components/NewTapForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+import { v4 } from "uuid";
+
+function NewTapForm(props) {
+  function handleNewTapFormSubmission(event){
+    event.preventDefault();
+    props.onNewTapCreation({
+      name: event.target.name.value,
+      abv: parseFloat(event.target.abv.value),
+      price: parseFloat(event.target.price.value),
+      description: (event.target.description.value),
+      id: v4(),
+    });
+  }
+
+  return(
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewTapFormSubmission}
+        buttonText="Add Tap"
+      />
+    </React.Fragment>
+  );
+}
+
+NewTapForm.propTypes = {
+  oneNewTapCreation: PropTypes.func,
+};
+
+export default NewTapForm;
