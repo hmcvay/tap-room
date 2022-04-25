dbfbb96d0af255dc40168fab51663fbf86c0ba6f
hmcvay <hayleymcvay8@gmail.com>
2022-04-25 10:37:49 -0700

    WIP: refactoring for  edit functionality & fix syntax/indent errors

--- src/components/TapControl.js
+++ src/components/TapControl.js
@@ -4,7 +4,7 @@ import NewTapForm from "./NewTapForm";
 import TapDetails from "./TapDetails";
 import EditTapForm from "./EditTapForm";
 
-class TapControl extends React.Component {
+export class TapControl extends React.Component {
 
   constructor() {
     super();
@@ -28,74 +28,81 @@ class TapControl extends React.Component {
     };
   }
 
-    handleButtonClick = () => {
-      if(this.state.selectedTap !== null || this.state.formVisibleOnPage){
-        this.setState(() => ({
-          formVisibleOnPage: false,
-          selectedTap: null,
-          editing: false,
-        }));
-      } else {
-        this.setState(() => ({
-          formVisibleOnPage: true,
-          selectedTap: null,
-        }));
-      }
-    };
-
-    handleEditingClick = () => {
-      this.setState({ editing: true });
-    };
-
-    handleAddingNewTapToList = (newTap) => {
-      const newMainTapList = this.state.mainTapList.concat(newTap);
-      this.setState({
-        mainTapList: newMainTapList,
-        selectedTap: newTap,
-      });
-    };
-
-    handleChangeSelectedTap = (id) => {
-      const selectedTap = this.state.mainTapList.filter(
-        (tap) => tap.id === id
-      )[0];
-      this.setState({ selectedTap: selectedTap });
-    };
-
-    handleChangingBeersSold = (id, numberOfDrinks) => {
-      let soldTap = this.state.mainTapList.filter((tap) => tap.id === id)[0];
-      soldTap.beersSold += numberOfDrinks;
-      this.setState({
-        mainTapList: this.state.mainTapList
-          .filter((tap) => tap.id !== id)
-          .concat(soldTap),
-      });
-    };
-
-    handleTapEditingInList = (tapToEdit) => {
-      const editedMainTapList = this.state.mainTapList
-        .filter((tap) => tap.id !== this.state.selectedTap.id)
-        .concat(tapToEdit);
-      this.setState({
-        mainTapList: editedMainTapList,
-        editing: false,
+  handleButtonClick = () => {
+    if(this.state.selectedTap !== null || this.state.formVisibleOnPage){
+      this.setState(() => ({
+        formVisibleOnPage: false,
         selectedTap: null,
-      });
-    };
+        editing: false,
+      }));
+    } else {
+      this.setState(() => ({
+        formVisibleOnPage: true,
+        selectedTap: null,
+        editing: false,
+      }));
+    }
+  };
 
-    render() {
-      const currentTapList = this.state.mainTapList;
-      let currentlyVisibleState = null;
-      let descriptionButtonText = null;
-      if (this.state.formVisibleOnPage) {
-        currentlyVisibleState = (
-          <NewTapForm onNewTapCreation={this.handleAddingNewTapToList} />
-        );
-      } else {
-        descriptionButtonText = "Add Tap";
+  handleEditingClick = () => {
+    this.setState({ editing: true });
+  };
+
+  handleAddingNewTapToList = (newTap) => {
+    const newMainTapList = this.state.mainTapList.concat(newTap);
+    this.setState({
+      mainTapList: newMainTapList,
+      selectedTap: newTap,
+    });
+  };
+
+  handleChangeSelectedTap = (id) => {
+    const selectedTap = this.state.mainTapList.filter(
+      (tap) => tap.id === id
+    )[0];
+    this.setState({ selectedTap: selectedTap });
+  };
+
+  handleChangingBeersSold = (id, numberOfDrinks) => {
+    let soldTap = this.state.mainTapList.filter((tap) => tap.id === id)[0];
+    soldTap.beersSold += numberOfDrinks;
+    this.setState({
+      mainTapList: this.state.mainTapList
+        .filter((tap) => tap.id !== id)
+        .concat(soldTap),
+    });
+  };
+
+  handleTapEditingInList = (tapToEdit) => {
+    const editedMainTapList = this.state.mainTapList
+      .filter((tap) => tap.id !== this.state.selectedTap.id)
+      .concat(tapToEdit);
+    this.setState({
+      mainTapList: editedMainTapList,
+      editing: false,
+      selectedTap: null,
+    });
+  };
+
+  render() {
+    const currentTapList = this.state.mainTapList;
+    let currentlyVisibleState = null;
+    let descriptionButtonText = null;
+    if (this.state.editing) {
+      currentlyVisibleState = {
+        <EditTapForm />
+          tap={this.state.selectedTap}
+          onEditTap={this.handleTapEditingInList}
+      
       }
-    
-    
+    }
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = (
+        <NewTapForm onNewTapCreation={this.handleAddingNewTapToList} />
+      );
+    } else {
+      descriptionButtonText = "Add Tap";
+    }
     return (
       <React.Fragment>
         <TapList 
